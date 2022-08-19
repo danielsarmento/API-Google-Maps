a96cc44ae373788f2b9f1647ec23f2ef3a9149b1
Daniel Sarmento <danielsarmento@users.noreply.github.com>
2022-08-19 19:15:09 -0300

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 const routes = require('./routes/routes_geolocalizacao');// Importando as rotas 
-const PORT = 3000;
+const PORT = process.env.PORT;
 const connectDB = require('./dataBase/dataBase')
 connectDB()
 app.use(express.json());
